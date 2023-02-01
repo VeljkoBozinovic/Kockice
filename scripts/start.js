eb042dec1fe9d47ff2a09f918043345c9f81b4a9
VeljkoBozinovic <veljkobozinovic@gmail.com>
2023-02-01 15:09:34 +0100

    final

--- scripts/start.js
+++ scripts/start.js
@@ -4,6 +4,10 @@ const elInputs = document.querySelectorAll("input[type = 'text']");
 //Player names GAME
 const elP1Name = document.querySelector(".playerOneName");
 const elP2Name = document.querySelector(".playerTwoName");
+//Countdown
+const elCD = document.querySelector(".countdown");
+const elCDParagraph = document.querySelector(".countdown-p");
+let CD = 3;
 
 let nameOne;
 let nameTwo;
@@ -18,5 +22,23 @@ elBtnStart.addEventListener("click", (e) => {
     elInputs[1].value !== "" ? elInputs[1].value : elInputs[1].placeholder;
   elP2Name.innerHTML = nameTwo;
 
-  elStart.style.display = "none";
+  countdownTimer();
 });
+
+function countdownTimer() {
+  elCD.classList.remove("CD-hide");
+
+  let myInterval = setInterval(() => {
+    CD--;
+
+    if (CD < 1) {
+      elCDParagraph.innerHTML = "Start!";
+      setTimeout(() => {
+        clearInterval(myInterval);
+        elStart.style.display = "none";
+      }, 1000);
+    } else {
+      elCDParagraph.innerHTML = CD;
+    }
+  }, 1000);
+}
